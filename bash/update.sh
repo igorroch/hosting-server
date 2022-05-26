c48e28d2a89ba67b135a4becbbd01a3d3c5b44d6
igorroch <igorroch@yandex.ru>
2022-05-27 00:11:09 +0300

    Fixed variables issues

--- bash/update.sh
+++ bash/update.sh
@@ -2,11 +2,11 @@ pm2 list
 
 # cat ./servers.json | jq '. | map(.host, .port)' | jq @sh
 for row in $(cat ../servers.json | jq -r '.[] | @base64'); do
-  FOLDER = echo ${row} | base64 --decode | jq -r '.folder' 
-  DOMAIN = echo ${row} | base64 --decode | jq -r '.domain' 
-  PORT = echo ${row} | base64 --decode | jq -r '.port' 
+  FOLDER=$(echo ${row} | base64 --decode | jq -r '.folder')
+  DOMAIN=$(echo ${row} | base64 --decode | jq -r '.domain')
+  PORT=$(echo ${row} | base64 --decode | jq -r '.port')
 
-  cd ../$FOLDER
+  cd $FOLDER
   git pull
   npm install
   pm2 delete -s $DOMAIN || :
